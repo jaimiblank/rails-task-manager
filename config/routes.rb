e7b888895d42ad377cc6ceba62f793cad6e9cd54
Jaimi Klein jaimiblankenship@gmail.com <jaimiblankenship@gmail.com>
2024-08-20 12:36:21 +0200

    added step 4 view the details

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/tasks", to: "tasks#index"
 
 # As a user, I can view the details of a task
+  get "/tasks/:id", to: "tasks#show", as: :show
 # As a user, I can add a new task
 # As a user, I can edit a task (mark as completed / update title & details)
 # As a user, I can remove a task
