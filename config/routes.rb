e51bc9cc3a4bae55f1291f1fab641b8ed4567406
Jaimi Klein jaimiblankenship@gmail.com <jaimiblankenship@gmail.com>
2024-08-20 12:10:12 +0200

    added step 3 can list a task

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+#   As a user, I can list tasks
+  get "/tasks", to: "tasks#index"
+
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
 end
